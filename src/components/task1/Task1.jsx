ef02321ff716dffc7507c2b08c56f74477a7d947
SushanGhimire <sushangmi55@gmail.com>
2022-03-23 09:08:07 +0545

    progress bar is added with fill property when button is clicked

--- src/components/task1/Task1.jsx
+++ src/components/task1/Task1.jsx
@@ -1,11 +1,13 @@
 import SliderRange from "./integrate/SliderRange";
 import ButtonLoader from "./integrate/ButtonLoader";
+import ProgressBar from "./integrate/ProgressBar";
 
 const Task1 = () => {
   return (
     <div className="flex flex-col p-10 items-center gap-4 justify-center">
       <SliderRange />
       <ButtonLoader />
+      <ProgressBar />
     </div>
   );
 };
