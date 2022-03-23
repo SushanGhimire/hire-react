d11e9c4e1b8d8059976cd0312f9392d28bdfe061
SushanGhimire <sushangmi55@gmail.com>
2022-03-23 08:42:57 +0545

    task-1  simple slider input with step-5 completed

--- src/components/task1/Task1.jsx
+++ src/components/task1/Task1.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
 const Task1 = () => {
-  return <div className="">Task1</div>;
+  const [sliderValue, setSliderValue] = useState(10);
+
+  const handleChange = (e) => {
+    setSliderValue(e.target.value);
+  };
+  return (
+    <div className="p-10">
+      <div className="flex items-center gap-4 justify-center">
+        <div class="flex flex-col">
+          <label htmlFor="">Slider Range</label>
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={sliderValue}
+            name="sliderValue"
+            step={5}
+            onChange={handleChange}
+          />
+          <span>{sliderValue}</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Task1;
