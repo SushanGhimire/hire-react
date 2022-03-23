4542993ea7edb9084cde4ad7fc14e16bf8a4f140
SushanGhimire <sushangmi55@gmail.com>
2022-03-23 11:00:45 +0545

    Barchat added (Package Apex Chart)

--- src/components/task3/Task3.jsx
+++ src/components/task3/Task3.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import BarChart from "./integrate/BarChart";
 
 const Task3 = () => {
   return (
-    <div>Task3</div>
-  )
-}
+    <div className="flex justify-center items-center p-10">
+      <BarChart />
+    </div>
+  );
+};
 
-export default Task3
+export default Task3;
---
+++ src/components/task3/integrate/BarChart.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Chart from "react-apexcharts";
+
+const BarChart = () => {
+  const [barDetail, setBarDetail] = useState({
+    options: {
+      chart: {
+        id: "basic-bar",
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+      },
+    },
+    series: [
+      {
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91],
+      },
+    ],
+  });
+  return (
+    <div className="">
+      <div className="row">
+        <div className="mixed-chart">
+          <Chart
+            options={barDetail.options}
+            series={barDetail.series}
+            type="bar"
+            width="500"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BarChart;
